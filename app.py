468aefb2fe67b6d6e621d43e38ce402e55946982
smithwithatypo <smithwithatypo@gmail.com>
2025-01-16 12:42:13 -0600

    trying post methods test1

--- app.py
+++ app.py
@@ -46,10 +46,14 @@ def add_message():
     if ENVIRONMENT == "production":
         if request.method == "POST":
             data = request.get_json()
-            new_message = Message(message=data['message'])
-            db.session.add(new_message)
-            db.session.commit()
-            return jsonify({"id": new_message.id, "message": new_message.message}), 201
+            return jsonify({'response': data})
+
+
+
+            # new_message = Message(message=data['message'])
+            # db.session.add(new_message)
+            # db.session.commit()
+            # return jsonify({"id": new_message.id, "message": new_message.message}), 201
         elif request.method == "GET":
             return jsonify({"error": "please send a POST request"})
     else:
