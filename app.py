95b0a717796e0f031de743591df09875eb71f835
smithwithatypo <smithwithatypo@gmail.com>
2025-01-16 12:01:38 -0600

    moved model around to init with db only in prod

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from dotenv import load_dotenv
 import os
+
+from models import Message
 
 # Load environment variables from .env file
 load_dotenv()
@@ -14,11 +16,14 @@ if ENVIRONMENT == "production":
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     db = SQLAlchemy(app)
     migrate = Migrate(app, db)
-    print("database url:", os.getenv("DATABASE_URL"))
-    pass
+
+    class Message(db.Model):
+        __tablename__ = 'test-table'
+        id = db.Column(db.Integer, primary_key=True)
+        message = db.Column(db.String, nullable=False)
+
 elif ENVIRONMENT == "development":
-    print("--dev mode found, no database connected--")
-    pass
+    print("-- dev mode found, no database connected --")
 else:
     print("environment variables did not load correctly")
     print("environment read: ", ENVIRONMENT)
@@ -28,21 +33,26 @@ else:
 def hello():
     return jsonify({"hello": "world"})
 
+
 @app.route('/api/get_messages')
 def get_messages():
     if ENVIRONMENT == "production":
-        class Message(db.Model):
-            __tablename__ = 'test-table'
-            id = db.Column(db.Integer, primary_key=True)
-            message = db.Column(db.String, nullable=False)
-
         messages = Message.query.all()
         return jsonify([{"id": message.id, "message": message.message} for message in messages])
     else:
-        return jsonify({"error": "Not in production environment"})
+        return jsonify({"error": "Not in production environment, no db connected"})
 
 
-
+@app.route('/api/add_message', methods=['POST'])
+def add_message():
+    if ENVIRONMENT == "production":
+        data = request.get_json()
+        new_message = Message(message=data['message'])
+        db.session.add(new_message)
+        db.session.commit()
+        return jsonify({"id": new_message.id, "message": new_message.message}), 201
+    else:
+        return jsonify({"error": "Not in production environment, no db connected"})
 
 
 if __name__ == '__main__':
